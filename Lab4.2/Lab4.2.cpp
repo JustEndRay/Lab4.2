f7cf72bd54d4fdc715e234b129a37f233680f1da
Andrew Pastushuk <andreypastushuk@gmail.com>
2020-10-25 19:31:26 +0200

    version 1.0

---
+++ Lab4.2/Lab4.2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;cout << "---------------------------"<< endl;
+	cout << "|"<< setw(5) << "x"<< "     |"<< setw(7) << "y"<< "       |"<< endl;
+	cout << "---------------------------"<< endl;
+	x = xp;
+	while(x <= xk)
+	{
+		A = 2+1./abs(1+x);
+		if(x<1)B = sqrt(abs(cos(x))+	13);
+		else
+			if(x>1&&x<5)B =3+tan(sqrt(2.)/(4+x)) ;
+			else
+				B = 8+0.7*x;
+		y = A + B;
+		cout << "|"<< setw(7) << setprecision(2) << x<< "   |"<< setw(10) << setprecision(3) << y<< "    |"<< endl;
+		x += dx;
+	}
+	cout << "---------------------------"<< endl;
+	return 0;
+}
